f8ecf27618efdbb9d65ca17af7e052e5d42b1ba3
PuckWang123 <37675715+PuckWang123@users.noreply.github.com>
2019-04-05 22:40:06 -0700

    update bot

--- Eclipse Project/Conversation.java
+++ Eclipse Project/Conversation.java
@@ -12,18 +12,12 @@ public class Conversation{
 	public String msg = "";
 	private static BDialog dialog = new BDialog();
 	private ArrayList<String> aList = new ArrayList<String>();
-	private ArrayList<String> randresp = new ArrayList<String>();
+
 	public Conversation(){
 		dialog.yeBot.setVisible(true);
 		aList.add("quit");
 		aList.add("see you");
 		aList.add("goodbye");
-		randresp.add("wish i could help   i dont know what that means");
-		randresp.add("you got good vibes   but i dont know what to say to that");
-		randresp.add("yo man you gotta slow down   maybe try saying that a different way");
-		randresp.add("it is out of my scope man");
-		randresp.add("that sounds interesting");
-		randresp.add("you got me there what is it about");
 	}
 	
 	//used to print and set an input
@@ -35,9 +29,6 @@ public class Conversation{
 		return result;
 	}
 
-	public boolean containedRandomResponse(String msg) {
-		return (msg != null) ? randresp.contains(msg.toLowerCase()) : false;
-	}
 	public String recieveInput() {
 		String result = dialog.recieveInput();
 		return result;
